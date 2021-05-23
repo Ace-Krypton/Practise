e489d215d9ecade0802185674c1831826dc8c776
Ace-Krypton <ramizna@code.edu.az>
2021-05-23 17:22:09 +0400

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Loop
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //Console.WriteLine(i);
+            #endregion
+
+            #region Odd
+            //int m = 5;
+            //int n = 10;
+            //int result = 0;
+
+            //for (int i = m; i <= n; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        result = result + i;
+            //    }
+            //}
+
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Switch Case 
+            //int n = 20;
+            //int m = 5;
+            //char process = '/';
+
+            //switch (process)
+            //{
+            //    case '/': 
+            //        Console.WriteLine(n / m); 
+            //        break;
+            //    case '*':
+            //        Console.WriteLine(n * m);
+            //        break;
+            //    case '+':
+            //        Console.WriteLine(n + m);
+            //        break;
+            //    case '-':
+            //        Console.WriteLine(n - m);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Please enter again:");
+            //        break;
+            //}
+
+            //if (process == '/')
+            //{
+            //    Console.WriteLine(n / m);
+            //}
+
+            //else if (process == '*')
+            //{
+            //    Console.WriteLine(n * m);
+            //}
+
+            //else if (process == '+')
+            //{
+            //    Console.WriteLine(n + m);
+            //}
+
+            //else if (process == '-')
+            //{
+            //    Console.WriteLine(n - m);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Please enter again");
+            #endregion
+
+            #region Array
+
+            //int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+
+            //int[] arr = new int[5];
+
+            //arr[0] = 5;
+            //arr[1] = 1;
+            //arr[2] = 8;
+            //arr[3] = 98;
+            //arr[4] = 7;
+
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //int[] arr = new int[] { 12, 24, 33 };
+            //int sum = 0;
+            //double result = 0;
+
+            //foreach (int item in arr)
+            //{
+            //    sum += item;
+            //}
+
+            //result = sum / arr.Length;
+            //Console.WriteLine(result);
+
+            //int[] arr = new int[] { 12, 14, 15, 67 };
+            //int max = arr[0];
+            //int min = arr[0];
+
+            //foreach (int item in arr)
+            //{
+            //    if (item > max)
+            //        max = item;
+            //    if (item < min)
+            //        min = item;
+            //}
+
+            //Console.WriteLine(max + min);
+
+            //int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine(arr.Average());
+            //Console.WriteLine(arr.Min());
+            //Console.WriteLine(arr.Max());
+            //Console.WriteLine(arr.Sum());
+            //Array.Sort(arr);
+
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //bool isExist = arr.Contains(200);
+
+            //Console.WriteLine(isExist);
+
+            //Console.Read();
+
+            #endregion
+
+
+
+        }
+
+    }
+}
